e9ab14708efcd775525673f4e11b19431fcca36e
David <dakratz@gmx.de>
2022-07-13 17:10:18 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,9 +12,16 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "basicwebapp12";
-        } else { // TODO extend the programm here
-            return "";
+           return "David";
+        } else if(query.contains("which")) {
+            String numbers = query.substring(query.indexOf("largest:")+8);
+            String[] num = numbers.split(",");
+            if(num.length < 2){ return "0";}
+            int max = Math.max(Integer.parseInt(num[0]),Integer.parseInt(num[1]));
+            return max + "";
+        } else{ // TODO extend the programm here
+                return "";
+
         }
     }
 }
